f93d74effc56877ed78cd320c7692a90cb9ad524
Wiktor Siatkowski <wik.siatprywatnie@gmail.com>
2018-07-24 12:57:57 +0200

    init

---
+++ desktop/src/com/bbgplatformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bbgplatformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bbgplatformer.BBGplatformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BBGplatformer(), config);
+	}
+}
