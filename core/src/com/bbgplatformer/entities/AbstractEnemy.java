821688b8a19bba1acb632e9574473d0af0c4c39b
Wiktor Siatkowski <wik.siatprywatnie@gmail.com>
2018-08-02 15:57:32 +0200

    Removed typo from name of class

--- core/src/com/bbgplatformer/entities/AbstractEnemy.java
+++ core/src/com/bbgplatformer/entities/AbstractEnemy.java
@@ -6,11 +6,11 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
-public abstract class AbstractEnnemy extends Image {
+public abstract class AbstractEnemy extends Image {
 	
 	protected Drawable leftDrawable, rightDrawable;
 	
-	public AbstractEnnemy() {
+	public AbstractEnemy() {
 		super();
 		init();
 	}
