188e7dc08bb222e46b078181d924ef71bb803a5c
Wiktor Siatkowski <wik.siatprywatnie@gmail.com>
2018-08-05 15:53:00 +0200

    Created level one map

--- core/src/com/bbgplatformer/screens/GameplayScreen.java
+++ core/src/com/bbgplatformer/screens/GameplayScreen.java
@@ -23,10 +23,10 @@ public class GameplayScreen extends AbstractScreen {
 	private int hp;
 	private MarshmallowMonsterEnemy monster;
 	private Music bgMusic;
-	
+
 	private final int CAMERA_Y = 300;
-	
-	public final int PLATFORMS_AMOUNT = 200;
+
+	public final int PLATFORMS_AMOUNT = 720;
 
 	public GameplayScreen(BBGplatformer game) {
 		super(game);
@@ -63,10 +63,193 @@ public class GameplayScreen extends AbstractScreen {
 	private void platformInit() {
 		int x = 0;
 		int y = 150;
+		
+		// am[x] - am[x-1] = amount of single platforms in one bigger with index x
+		int[] am = {36,50,100,101,103,150,152,220,250,280,360,400,420,440,510,550,590,620,720};
 
 		platforms = new Platform[PLATFORMS_AMOUNT];
 
-		for (int i = 0; i < PLATFORMS_AMOUNT; i++) {
+		for (int i = 0; i < am[0]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 96;
+		y += 64;
+		for (int i = am[0]; i < am[1]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 130;
+		y = 150;
+		for (int i = am[1]; i < am[2]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 96;
+		y += 55;
+		for (int i = am[2]; i < am[3]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 150;
+		y += 64;
+		for (int i = am[3]; i < am[4]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 100;
+		y += 40;
+		for (int i = am[4]; i < am[5]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 64;
+		y -= 40;
+		for (int i = am[5]; i < am[6]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 110;
+		y = 150;
+		for (int i = am[6]; i < am[7]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 90;
+		y += 60;
+		for (int i = am[7]; i < am[8]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 90;
+		y += 60;
+		for (int i = am[8]; i < am[9]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 240;
+		y = 150;
+		for (int i = am[9]; i < am[10]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 90;
+		y += 70;
+		for (int i = am[10]; i < am[11]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 90;
+		y += 64;
+		for (int i = am[11]; i < am[12]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 90;
+		y += 55;
+		for (int i = am[12]; i < am[13]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 90;
+		y += 50;
+		for (int i = am[13]; i < am[14]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 180;
+		y = 180;
+		for (int i = am[14]; i < am[15]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 96;
+		y += 60;
+		for (int i = am[15]; i < am[16]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 96;
+		y += 60;
+		for (int i = am[16]; i < am[17]; i++) {
+			platforms[i] = new Platform(platformImage, x, y);
+
+			platforms[i].setX(x);
+			platforms[i].setY(y);
+			stage.addActor(platforms[i]);
+			x += platformImage.getWidth();
+		}
+		x += 200;
+		y  = 150;
+		for (int i = am[17]; i < am[18]; i++) {
 			platforms[i] = new Platform(platformImage, x, y);
 
 			platforms[i].setX(x);
@@ -88,7 +271,7 @@ public class GameplayScreen extends AbstractScreen {
 			stage.addActor(hpImage[i]);
 		}
 	}
-	
+
 	private void musicInit() {
 		bgMusic = Gdx.audio.newMusic(Gdx.files.internal("bgMusic.ogg"));
 		bgMusic.setLooping(true);
@@ -96,7 +279,7 @@ public class GameplayScreen extends AbstractScreen {
 	}
 
 	private void monstersInit() {
-		monster = new MarshmallowMonsterEnemy(600,182,400);
+		monster = new MarshmallowMonsterEnemy(600, 182, 400);
 		stage.addActor(monster);
 	}
 
@@ -166,7 +349,7 @@ public class GameplayScreen extends AbstractScreen {
 	}
 
 	private void monsterUpdate() {
-			monster.move();
+		monster.move();
 	}
 
 	private void subtractHp() {
